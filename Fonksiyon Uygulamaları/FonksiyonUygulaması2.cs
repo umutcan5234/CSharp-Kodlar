722335042f348c2511c0dd8accf6ee4f619c2308
Umut Can <umutcanmert8@gmail.com>
2025-04-24 19:22:31 +0000

    Upload New File

---
+++ Fonksiyon Uygulamaları/FonksiyonUygulaması2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int faktoriyel(int sayi)  
+            {
+                int x = 1;  
+                for (int i = 1; i <= sayi; i++)  
+                {
+                    x = x * i; 
+                }
+                return x;  
+            }
+            {
+                Console.Write("Bir sayı giriniz: ");  
+                int sayi = Convert.ToInt32(Console.ReadLine());  
+                int sonuc = faktoriyel(sayi); 
+                Console.WriteLine(sayi + "! = " + sonuc); 
+            }
+            Console.ReadKey();  
+        }
+    }
+}
